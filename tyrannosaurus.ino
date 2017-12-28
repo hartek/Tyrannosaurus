22684f4bb9acc52bddabb8659ad5caa171d7accf
hartek <guiroma93@hotmail.com>
2017-12-28 01:43:21 +0100

    Code and sketch added

---
+++ tyrannosaurus.ino
@@ -0,0 +1,48 @@
+int led = 6;
+int led2 = 10;
+int pir = 8; 
+int buzz = 9; 
+
+int motor_1_delante = 2;
+int motor_1_detras = 3; 
+int motor_1_pwm = 5; 
+
+void setup() {                
+  pinMode(led, OUTPUT);  
+  pinMode(led2, OUTPUT);  
+  pinMode(pir, INPUT);     
+  pinMode(buzz, OUTPUT);    
+  
+  pinMode(motor_1_delante, OUTPUT);
+  pinMode(motor_1_detras, OUTPUT); 
+  pinMode(motor_1_pwm, OUTPUT); 
+  
+  analogWrite(motor_1_pwm, 255); 
+}
+
+void loop() {
+  int pir_value = digitalRead(pir); 
+  //int pir_value = HIGH; 
+  if (pir_value == HIGH) {
+    digitalWrite(led, HIGH); 
+    digitalWrite(led2, LOW); 
+    tone(buzz, 1976); 
+    digitalWrite(motor_1_delante, HIGH); 
+    digitalWrite(motor_1_detras, LOW); 
+    delay(250); 
+    
+    digitalWrite(led, LOW); 
+    digitalWrite(led2, HIGH); 
+    tone(buzz, 1865); 
+    digitalWrite(motor_1_delante, LOW); 
+    digitalWrite(motor_1_detras, HIGH); 
+    delay(250); 
+  }
+  else {
+    digitalWrite(led, LOW); 
+    digitalWrite(led2, LOW); 
+    digitalWrite(motor_1_delante, LOW); 
+    digitalWrite(motor_1_detras, LOW); 
+    noTone(buzz); 
+  }
+}
